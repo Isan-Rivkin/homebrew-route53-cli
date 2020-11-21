a56abbcad594f112bb69076e96d029f5820b68e2
Isan-Rivkin <isanrivkin@gmail.com>
2020-11-21 20:19:22 +0200

    route53-cli (new formula)

---
+++ Formula/route53-cli.rb
@@ -0,0 +1,17 @@
+class Route53Cli < Formula
+  desc "Route53 cli utility tools for quick querying all those tedious tasks"
+  homepage ""
+  url "https://github.com/Isan-Rivkin/route53-cli/releases/download/v0.1.2/route53-cli_0.1.2_darwin_amd64.tar.gz"
+  sha256 "2feda75a55f9ebf3cb2ab58e975af57606787cfba41b67d7d7f92062ca145fdc"
+  license "Apache-2.0"
+
+  depends_on "go" => :build
+
+  def install
+    system "go", "build", *std_go_args
+  end
+
+  test do
+  	assert_match "Run as expected", shell_output(bin/"route53-cli", 0)
+  end
+end
