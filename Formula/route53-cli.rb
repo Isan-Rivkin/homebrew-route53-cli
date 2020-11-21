ac2aacf09283fffaec2b05d1fe0fc516ec6b498c
Isan-Rivkin <isanrivkin@gmail.com>
2020-11-21 20:28:43 +0200

    some fixeS

--- Formula/route53-cli.rb
+++ Formula/route53-cli.rb
@@ -1,6 +1,6 @@
 class Route53Cli < Formula
-  desc "Route53 cli utility tools for quick querying all those tedious tasks"
-  homepage ""
+  desc "A cli tool for Route53 to quickly query values of records"
+  homepage "https://github.com/Isan-Rivkin/route53-cli"
   url "https://github.com/Isan-Rivkin/route53-cli/releases/download/v0.1.2/route53-cli_0.1.2_darwin_amd64.tar.gz"
   sha256 "2feda75a55f9ebf3cb2ab58e975af57606787cfba41b67d7d7f92062ca145fdc"
   license "Apache-2.0"
@@ -12,6 +12,6 @@ class Route53Cli < Formula
   end
 
   test do
-  	assert_match "Run as expected", shell_output(bin/"route53-cli", 0)
+    assert_match "Run as expected", shell_output(bin/"route53-cli")
   end
 end
